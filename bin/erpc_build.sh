20f358bff4cc95fd84384cacb8d3a660dfd13fb6
kazuhiro MIYASHITA <miyakz1192@gmail.com>
2020-06-17 13:08:55 +0000

    erpc/

--- bin/erpc_build.sh
+++ bin/erpc_build.sh
@@ -1,3 +1,3 @@
 echo "do this on rails root dir"
 set -x
-grpc_tools_ruby_protoc --ruby_out=./ruby --grpc_out=./ruby erpc/sentence.proto
+grpc_tools_ruby_protoc --ruby_out=. --grpc_out=. erpc/sentence.proto
